109675a42a2bb473347d29bb75b74c24eaf19e66
l1xxin <2373480864@qq.com>
2023-04-23 21:22:28 +0800

    教務進行活動管理的前端vue文件

---
+++ vue/src/views/ZeActManage.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ZeActManage",
+  data() {
+    return {
+      tableData: [],
+      total: 0,
+      pageNum: 1,
+      pageSize: 10,
+      name: "",
+      form: {},
+      dialogFormVisible: false,
+      multipleSelection: [],
+      user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
+    }
+  },
+  created() {
+    this.load()
+  },
+  methods: {
+    load() {
+      
+    },
+    
+    handleAdd() {
+      this.dialogFormVisible = true
+      this.form = {}
+      this.$nextTick(() => {
+        if(this.$refs.img) {
+           this.$refs.img.clearFiles();
+         }
+         if(this.$refs.file) {
+          this.$refs.file.clearFiles();
+         }
+      })
+    },
+    handleEdit(row) {
+      this.form = JSON.parse(JSON.stringify(row))
+      this.dialogFormVisible = true
+       this.$nextTick(() => {
+         if(this.$refs.img) {
+           this.$refs.img.clearFiles();
+         }
+         if(this.$refs.file) {
+          this.$refs.file.clearFiles();
+         }
+       })
+    },
+    
+    handleSelectionChange(val) {
+      console.log(val)
+      this.multipleSelection = val
+    },
+    
+    reset() {
+      this.name = ""
+      this.load()
+    },
+    handleSizeChange(pageSize) {
+      console.log(pageSize)
+      this.pageSize = pageSize
+      this.load()
+    },
+    handleCurrentChange(pageNum) {
+      console.log(pageNum)
+      this.pageNum = pageNum
+      this.load()
+    },
+    handleFileUploadSuccess(res) {
+      this.form.file = res
+    },
+    handleImgUploadSuccess(res) {
+      this.form.img = res
+    },
+    download(url) {
+      window.open(url)
+    },
+    
+    handleExcelImportSuccess() {
+      this.$message.success("导入成功")
+      this.load()
+    }
+  }
+}
+</script>
+
+
+<style>
+.headerBg {
+  background: #eee!important;
+}
+</style>
