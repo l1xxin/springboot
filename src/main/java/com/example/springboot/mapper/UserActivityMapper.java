31d541ae1f150f86dafcbea73287bb9023831b27
l1xxin <2373480864@qq.com>
2023-04-23 20:47:47 +0800

    编写用于教务为活动添加用户的mapper sql语句

--- src/main/java/com/example/springboot/mapper/UserActivityMapper.java
+++ src/main/java/com/example/springboot/mapper/UserActivityMapper.java
@@ -1,7 +1,13 @@
 package com.example.springboot.mapper;
 
+import com.example.springboot.controller.dto.ActUserDTO;
+import com.example.springboot.entity.User;
 import com.example.springboot.entity.UserActivity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +18,13 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2023-04-23
  */
 public interface UserActivityMapper extends BaseMapper<UserActivity> {
+
+    @Select("SELECT s.username as num,s.nickname as name\n" +
+            "FROM sys_user s\n" +
+            "WHERE NOT EXISTS(\n" +
+            "SELECT 1 FROM user_activity u\n" +
+            "WHERE s.username = u.user_id and u.act_id = #{actId}\n" +
+            ") AND s.role = 'ROLE_STU'")
+    List<ActUserDTO> selectByActId(@Param("actId") Integer actId);
 
 }
